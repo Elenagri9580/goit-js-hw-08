3d5a41f7c40e575b87533cf701d1ba6b0a3e9fac
Elenagri9580 <150274791+Elenagri9580@users.noreply.github.com>
2024-03-22 17:45:50 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -76,21 +76,21 @@ function createGallery(arr) {
     .map(
       ({ preview, original, description }) => `
         <li class="gallery-item">
-            <a class="gallery-link" href="${original}">
-                <img
-                  class="gallery-image"
-                  src="${preview}"
-                  data-source="${original}"
-                  alt="${description}"
-                />
-            </a>
+          <a class="gallery-link" href="${original}">
+            <img
+              class="gallery-image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
         </li>`
     )
     .join('');
 }
 
 function handleClick(event) {
-  event.preventDefault();  
+  event.preventDefault();
   if (event.target === event.currentTarget) {
     return;
   }
